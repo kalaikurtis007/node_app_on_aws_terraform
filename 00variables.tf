d6c0f73cc6bc0da3ca0790666f27006f112d0a56
kalaikurtis007 <kalaikurtis@gmail.com>
2023-08-06 07:49:44 +0530

    udpated variable

--- 00variables.tf
+++ 00variables.tf
@@ -7,3 +7,8 @@ variable "encrypt_key" {
   type    = string
   default = "ec2_key"
 }
+
+variable "application_ami" {
+  type    = "string"
+  default = "ami-0f34c5ae932e6f0e4" // free tier amazon linux latest.
+}
